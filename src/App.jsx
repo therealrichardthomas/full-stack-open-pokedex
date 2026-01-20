2c8149c03acb1823ff300205cdc83d3f4883cc6d
Richard Thomas <richuthomasp@gmail.com>
2026-01-20 18:25:27 -0500

    fixed linting errors 11.6

--- src/App.jsx
+++ src/App.jsx
@@ -33,12 +33,14 @@ const App = () => {
   }
 
   return (
-    <Routes>
-      <Route exact path="/" element={<PokemonList pokemonList={pokemonList} />} />
-      <Route exact path="/pokemon/:name" element={
-        <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
-      } />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<PokemonList pokemonList={pokemonList} />} />
+        <Route exact path="/pokemon/:name" element={
+          <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
+        } />
+      </Routes>
+    </Router>
   )
 }
 
