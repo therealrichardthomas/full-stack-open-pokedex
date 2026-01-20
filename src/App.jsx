b5859c08d925279c62300347dbc0e6ae549714d0
Richard Thomas <richuthomasp@gmail.com>
2026-01-20 18:34:17 -0500

    11.8: Fixed code errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, useMatch } from 'react-router-dom'
+import { Routes, Route, useMatch } from 'react-router-dom'
 import { useApi } from './useApi'
 import LoadingSpinner from './LoadingSpinner'
 import ErrorMessage from './ErrorMessage'
@@ -33,14 +33,12 @@ const App = () => {
   }
 
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<PokemonList pokemonList={pokemonList} />} />
-        <Route exact path="/pokemon/:name" element={
-          <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
-        } />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route exact path="/" element={<PokemonList pokemonList={pokemonList} />} />
+      <Route exact path="/pokemon/:name" element={
+        <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
+      } />
+    </Routes>
   )
 }
 
