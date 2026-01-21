78eea3ad05141de5feddc8d28b598a78c4499127
Richard Thomas <richuthomasp@gmail.com>
2026-01-20 19:07:02 -0500

    11.9.1

---
+++ e2e-tests/Pokedex.spec.js
@@ -0,0 +1,11 @@
+
+// eslint-disable-next-line no-unused-vars
+import { test, describe, expect, beforeEach } from '@playwright/test'
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+})
