858ce36cbdbe68eff83443424d8b93f8e57ea865
Richard Thomas <richuthomasp@gmail.com>
2026-01-21 22:28:03 -0500

    11.12: Fixed code

--- app.js
+++ app.js
@@ -12,9 +12,8 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
-  // console.log('Health Check Successful')
+  // eslint-disable-next-line no-console
+  console.log('Health Check Successful')
   res.send('ok')
 })
 
